4445dbeb6d39f04b9d26c7cea89400876f07e3b9
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-30 15:26:37 +0200

    fix: dashboard url

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware(['auth', 'verified'])
 ->name('admin.')
 ->prefix('admin')
 ->group(function() {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('projects', ProjectController::class);
 });
 
